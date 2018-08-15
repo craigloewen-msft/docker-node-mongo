9a28107f79ce21823ea3ede034cc2ac8d5cfb816
Brad Traversy <traversymedia@gmail.com>
2018-08-14 21:32:41 -0400

    Added views and license

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const redis = require("redis");
 
 const app = express();
 
@@ -19,10 +20,48 @@ mongoose
 
 const Item = require('./models/Item');
 
+// Connect to REdis too 
+
+var client = redis.createClient('6379', 'redis');
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+// Start loading in 100s of items to the database
+
+var countValue = 1;
+
+var addItemFunction = function () {
+  var someItem = new Item({ name: "genericItem" + countValue });
+  if (countValue > 500) {
+    console.log("Finished adding in tons of items")
+  } else {
+    someItem.save();
+  }
+  countValue++;
+}
+
+Item.countDocuments(function (e, totalItems) {
+  if (totalItems < 10) {
+    for (let i = 0; i < 10; i++) {
+      console.log("Adding item");
+      addItemFunction();
+    }
+  }
+});
+
 app.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+  client.incr('counter', function (err, counter) {
+    if (err) return next(err);
+    // Choose some random items to show
+    Item.countDocuments(function (e, totalItems) {
+      Item.find().sort({ _id: -1 })
+        // Item.findById()
+        .then(items => res.render('index', { items, counter, totalItems }))
+        .catch(err => res.status(404).json({ msg: 'No items found' }));
+    });
+  })
 });
 
 app.post('/item/add', (req, res) => {
